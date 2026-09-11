9202e6d8034c492fe708cf92845743ba721c2e76
pachamuthu <pacha4655@gmail.com>
2026-09-11 19:00:53 +0530

    Eliminate unnecessary vertical whitespace below login card

--- src/App.tsx
+++ src/App.tsx
@@ -485,10 +485,8 @@ const MainAppContent: React.FC = () => {
   // If user is not logged in, show Responsive Welcome & Onboarding flow
   if (!user) {
     return (
-      <div className="min-h-[100dvh] w-full bg-[#F3F7F5] flex flex-col items-center justify-start sm:justify-center px-3 py-4 sm:px-6 sm:py-8 lg:p-10 overflow-y-auto pt-safe pb-safe font-sans">
-        <div className="w-full flex-1 flex flex-col items-center justify-center my-auto">
-          <WelcomeScreen />
-        </div>
+      <div className="min-h-[100dvh] w-full bg-[#F3F7F5] flex flex-col items-center justify-center px-3 py-4 sm:px-6 sm:py-8 lg:p-10 font-sans">
+        <WelcomeScreen />
         <ToastContainer />
       </div>
     );
